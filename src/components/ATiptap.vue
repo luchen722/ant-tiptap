0d915cb261da9b0014c0d7a8943c7d577a8c8466
wmh <wmh@fccs.com>
2024-02-04 15:31:48 +0800

    chore: 宽度；lang 没用

--- src/components/ATiptap.vue
+++ src/components/ATiptap.vue
@@ -116,10 +116,6 @@ export default defineComponent({
       type: String,
       default: '',
     },
-    lang: {
-      type: String,
-      default: 'en',
-    },
     width: {
       type: [String, Number],
       default: undefined,
